1a92d40e253d73430e38f4f28c8fc604df7f7f8d
CagriSh <104763269+CagriSh@users.noreply.github.com>
2022-07-24 21:54:08 +0200

    Telephone example

--- src/fieldTypes/Telephone.java
+++ src/fieldTypes/Telephone.java
@@ -1,11 +1,6 @@
 package fieldTypes;
 
 public class Telephone {
-
-
-
-}
-
 /*
     Write the definition of a class Telephone.
 The class has no constructors, one instance variable of type String called number, and two public static variables.
@@ -13,3 +8,39 @@ One is of type int called quantity, initialized to 250;
 the other is of type double called total, initialized to 15658.92.
 Create getNumber() and setNumber() methods that will manipulate with number instance variable.
      */
+
+    public String number;
+
+    public static int quantity;
+
+    public static double total;
+
+    static{
+        quantity=250;
+        total= 1568.92;
+    }
+
+    public void setNumber(String number){
+        this.number=number;
+
+    }
+
+    public String getNumber(){
+        return number;
+    }
+
+    public static void main(String[] args) {
+        Telephone telephone=new Telephone();
+
+        System.out.println(telephone.total);
+        
+        telephone.setNumber("555-4536768");
+
+        System.out.println("telephone.getNumber() = " + telephone.getNumber());
+
+        System.out.println("telephone.total = " + telephone.total);
+
+    }
+    
+}
+
